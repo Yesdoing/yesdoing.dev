153f5c17198ca150cc6aa6173a7d4c0723da61e3
Jane Lee <Yesdoing13@gmail.com>
2019-09-21 18:56:20 +0900

    refactor: change social url

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -5,10 +5,10 @@ module.exports = {
   introduction: `Personal blog by Yesdoing. I write words and code to my mind.`,
   siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
   social: {
-    twitter: `https://twitter.com/yesdoing1`, // Your Twitter account
-    github: `https://github.com/yesdoing`, // Your GitHub account
+    twitter: `yesdoing1`, // Your Twitter account
+    github: `yesdoing`, // Your GitHub account
     medium: ``, // Your Medium account
-    facebook: `https://facebook.com/yesdoing`, // Your Facebook account
+    facebook: `yesdoing`, // Your Facebook account
   },
   icon: `content/assets/icon.png`, // Add your favicon
   keywords: [`blog`],
